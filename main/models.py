cad461a15feb1d9f0eee3855aacf8e6eec291fdf
AzfarZaidi <azfar.zaidi142@gmail.com>
2016-11-16 23:08:48 +0530

    Updated models

--- main/models.py
+++ main/models.py
@@ -1,10 +1,13 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Department(models.Model):
 	dept_id = models.CharField(max_length=16, primary_key=True)
 	name = models.CharField(max_length=64)
-	head_id = models.CharField(max_length=16, default='0')
+	head_id = models.CharField(max_length=16, default='NULL')
+
+	def __str__(self):
+		return '' + self.name
 	# causes problem when defining foreign key on Teacher
 	# as it wouldn't exist when this statement is executed
 
@@ -15,17 +18,26 @@ class Teacher(models.Model):
 	salary = models.FloatField()
 	teaching_since = models.DateField()
 
+	def __str__(self):
+		return '' + self.name
+
 class Student(models.Model):
 	student_id = models.CharField(max_length=16, primary_key=True)
 	roll_no = models.CharField(max_length=16)
 	name = models.CharField(max_length=64)
 	dob = models.DateField()
-	semester = models.IntegerField()
+	semester = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(8)])
 	dept_id = models.ForeignKey(Department)
 
+	def __str__(self):
+		return '' + self.name
+
 class StudentSection(models.Model):
-	student = models.OneToOneField(Student, on_delete=models.CASCADE)
+	student_id = models.OneToOneField(Student, on_delete=models.CASCADE)
 	section = models.CharField(max_length=1)
+
+	def __str__(self):
+		return '{} '.format(self.student_id) + 'is in section {}'.format(self.section)
 
 class Subject(models.Model):
 	subject_id = models.CharField(max_length=16, primary_key=True)
@@ -33,15 +45,24 @@ class Subject(models.Model):
 	semester = models.IntegerField()
 	dept_id = models.ForeignKey(Department)
 
+	def __str__(self):
+		return '' + self.name
+
 class SubjectSection(models.Model):		# weak relation
 	subject_id = models.ForeignKey(Subject)
 	section = models.CharField(max_length=1)
+
+	def __str__(self):
+		return '{}'.format(self.subject_id) + ' is taught in section {}'.format(self.section)
 
 class Teaches(models.Model):
 	teacher_id = models.ForeignKey(Teacher)
 	subject_id = models.ForeignKey(Subject)
 	class Meta:
 		unique_together = ('teacher_id', 'subject_id')
+
+	def __str__(self):
+		return '{} '.format(self.teacher_id) + ' teaches {}'.format(self.subject_id)
 
 class ClassAttendance(models.Model):
 	student_id = models.ForeignKey(Student)
@@ -50,6 +71,9 @@ class ClassAttendance(models.Model):
 	class Meta:
 		unique_together = ('student_id', 'subject_id')
 
+	def __str__(self):
+		return '{} {}'.format(self.student_id, self.attendance)
+
 class Marks(models.Model):
 	student_id = models.ForeignKey(Student)
 	subject_id = models.ForeignKey(Subject)
@@ -57,8 +81,14 @@ class Marks(models.Model):
 	class Meta:
 		unique_together = ('student_id', 'subject_id')
 
+	def __str__(self):
+		return '{} - {}'.format(self.student_id, self.subject_id)
+
 class Schedule(models.Model):
 	subject_id = models.ForeignKey(Subject)
 	date = models.DateField()
 	class Meta:
 		unique_together = ('subject_id', 'date')
+
+	def __str__(self):
+		return '{} - {}'.format(self.subject_id, self.date)
